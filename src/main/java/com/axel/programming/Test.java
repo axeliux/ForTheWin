256c226ed076d190b4f23327134c9fc991c36bbd
Axel Velazquez <axvelaz@microsoft.com>
2016-08-30 11:33:48 -0700

    Creating the skeleton for a Java Project

---
+++ src/main/java/com/axel/programming/Test.java
@@ -0,0 +1,7 @@
+package com.axel.programming;
+
+public class Test {
+	public static void hola(int x) {
+		System.out.println("***Here is just a test = " + x);
+	}
+}
---
+++ src/test/java/com/axel/programming/TestTest.java
@@ -0,0 +1,15 @@
+import com.axel.programming.*;
+import org.junit.Test;
+import java.util.ArrayList;
+
+public class TestTest {
+	@Test
+	public void test1() {
+		com.axel.programming.Test.hola(1);
+	}
+
+	@Test
+	public void test2() {
+		com.axel.programming.Test.hola(2);
+	}
+}
